516a4e3f86aaccb712b7bad13e187bbd28b5fc2e
denis shockwave <mwitidenis4@gmail.com>
2016-03-28 12:12:56 +0300

    Update validation.py

--- validation.py
+++ validation.py
@@ -45,106 +45,14 @@ class Validation:
                 return rows
             except mysql.connector.Error as err:
 
-                print "denis"
-        else:
-            print "CUrsor is none"
-            
-    def check_exist_state(self,query,event_code):
-        if(self.connection==None):
-            print "No connection"
-            return
-        self.cursor=self.connection.cursor()
-        if(self.cursor!=None):
-            try:
-                self.cursor.execute(query,(event_code,))
-                self.cursor.fetchall()
-                rowCount=self.cursor.rowcount
-                return rowCount
-            except mysql.connector.Error as err:
-
-                print err
-        else:
-            print "CUrsor is none"
-    def get_with_phone(self,query,phone_no):
-        if(self.connection==None):
-            print "No connection"
-            return
-        self.cursor=self.connection.cursor()
-        if(self.cursor!=None):
-            try:
-                self.cursor.execute(query,(phone_no,))
-                self.cursor.fetchall()
-                rowCount=self.cursor.rowcount
-                return rowCount
-            
-            except mysql.connector.Error as err:
-
                 print err
         else:
             print "CUrsor is none"
             
-    def check_user_booked(self,query,phone_no,event_code):
-        if(self.connection==None):
-            print "No connection"
-            return
-        self.cursor=self.connection.cursor()
-        if(self.cursor!=None):
-            try:
-                self.cursor.execute(query,(phone_no,event_code))
-                self.cursor.fetchall()
-                return self.cursor.rowcount
-            
-            
-            except mysql.connector.Error as err:
-
-                print err
-        else:
-            print "CUrsor is none"
-                    
-    def check_ticket_availability(self,query,event_code):
-        if(self.connection==None):
-            print "No connection"
-            return
-        self.cursor=self.connection.cursor()
-        if(self.cursor!=None):
-            try:
-                self.cursor.execute(query,(event_code,))
-                
-                for row in self.cursor:
-                    return row[0]
-                
-                
-            
-    
-            
-            
-            except mysql.connector.Error as err:
-
-                print err
-        else:
-            print "CUrsor is none"
-            
-    def insert_db(self,query,var1,var2,var3):
-        if(self.connection==None):
-            print "No connection"
-            return
-        self.cursor=self.connection.cursor()
-        if(self.cursor!=None):
-            try:
-                data=(var1,var2,var3)
-                self.cursor.execute(query,data)
-                self.connection.commit()
-                print "success" #add log info success
-    
-            except mysql.connector.Error as err:
-
-                print err
-        else:
-            print "CUrsor is none"
      
-    def create_random(self):
+    def create_random(self,begin,end):
 
-        first = random.randint(1,9)
+        first = random.randint(begin,end)
         first = str(first)
         n = 5
 
@@ -153,26 +61,10 @@ class Validation:
             first += str(nrs[i])
     
         return str(first)
-     
-    def store_user(self,query,name,phone_no):
-        if(self.connection==None):
-            print "No connection"
-            return
-        self.cursor=self.connection.cursor()
-        if(self.cursor!=None):
-            try:
-               self.cursor.execute(query,(name,phone_no))
-               self.connection.commit()
-       
-    
-            except mysql.connector.Error as err:
 
-                print err
-        else:
-            print "CUrsor is none"
         
     
-    def two_var(self,query,first,sec):
+    def three_var(self,query,first,sec):
         if(self.connection==None):
             print "No connection"
             return
@@ -190,47 +82,11 @@ class Validation:
         else:
             print "CUrsor is none"
         
-    def get_ticket_code(self,query,first,sec):
-        if(self.connection==None):
-            print "No connection"
-            return
-        self.cursor=self.connection.cursor()
-        if(self.cursor!=None):
-            try:
-               self.cursor.execute(query,(first,sec))
-               for row in self.cursor:
-                    return row[0]
-     
-    
-            except mysql.connector.Error as err:
 
-                print err
-        else:
-            print "CUrsor is none"
         
-        
-    def get_code(self,query,phone):
-        if(self.connection==None):
-            print "No connection"
-            return
-        self.cursor=self.connection.cursor()
-        if(self.cursor!=None):
-            try:
-                self.cursor.execute(query,(phone,))
-                if(self.cursor.rowcount !=None):
-                    for row in self.cursor:
-                        return row[0]
-                else:
-                    return None
-            
-            except mysql.connector.Error as err:
 
-                print err
-        else:
-            print "CUrsor is none"
             
-            
-    def one_var(self,query,value):
+    def two_var(self,query,value):
         if(self.connection==None):
             print "No connection"
             return
@@ -274,7 +130,7 @@ class Validation:
             
             
             
-    def one_var_update(self,query,value):
+    def two_var_update(self,query,value):
         if(self.connection==None):
             print "No connection"
             return
